bd80dbb880743109e01f657add7daf69787bdf0c
Dmit Bashmakov <denegniy1@gmail.com>
2024-07-17 15:04:59 +0500

    added utilities for data operations

---
+++ src/utils.py
@@ -0,0 +1,25 @@
+def get_salary():
+    salary = input('Введите желаемую зарплату в виде "минимальная зарплата - максимальная зарплата": ')
+    try:
+        min_salary_str, max_salary_str = salary.split(' - ')
+        min_salary = int(min_salary_str)
+        max_salary = int(max_salary_str)
+        if min_salary > max_salary:
+            raise ValueError
+        return min_salary, max_salary
+    except ValueError:
+        print('Диапазон введен неверно! Пожалуйста, введите суммы в виде'
+              '"минимальная зарплата - максимальная зарплата"')
+
+
+def get_vac_filtered_by_salary(vac_list):
+    vac_sorted = sorted(vac_list, key=lambda vac_list: vac_list.salary_from, reverse=True)
+    return vac_sorted
+
+
+def check_min_max_salary(vac_list, min_sal, max_sal):
+    result = []
+    for each in vac_list:
+        if min_sal <= each.salary_from <= max_sal and each.salary_to <= max_sal:
+            result.append(each)
+    return result
